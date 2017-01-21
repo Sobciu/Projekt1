d513ff95df99175572edd6a597da9d341187459a
Sobciu <sobciu312@gmail.com>
2017-01-21 13:48:55 +0100

    kolejna wersja pliku

--- plik.cpp
+++ plik.cpp
@@ -3,5 +3,11 @@ using namespace std;
 int main()
 {
     cout<<"Obsluga Gita"<<endl;
+    cout<<"Czy git dziala poprawnie?"<<endl;
+    int a;
+    cin >> a;
+    if (a == 1) cout<<"Wszystko dzia³a poprawnie"<<endl;
+        else cout<<"Cos poszlo nie tak"<endl;
+
     return 0;
 }
